1d7c14e1c266861a5e75077ddf6acb614c8f98e9
Lynix <81735347+lynixzenial@users.noreply.github.com>
2021-09-08 21:50:09 -0300

    Add Command Registeration

---
+++ core/make-commands.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { clientId, guildId, token } = require('./config.json');
+
+const commands = [
+    new SlashCommandBuilder().setName('uptime').setDescription('Replies with uptime!'),
+	new SlashCommandBuilder().setName('ping').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('server').setDescription('Replies with server info!'),
+	new SlashCommandBuilder().setName('user').setDescription('Replies with user info!'),
+    new SlashCommandBuilder().setName('sergal').setDescription('Easter Egg Command!'),
+].map(command => command.toJSON());
